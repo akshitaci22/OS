563de20e085f71503632c365fdbb285c02742f6a
akshitaci22 <100338124+akshitaci22@users.noreply.github.com>
2022-08-23 22:43:29 +0530

    Update fcfs1.cpp

--- fcfs1.cpp
+++ fcfs1.cpp
@@ -18,24 +18,29 @@ class FCFS
     }
     void findTurnAroundTime(int process[],int n,int ct[])
     {   
-        int tat[n];
+        int tat[n],average;
         cout<<"Turn around time:"<<endl;
         for(int i=0;i<n;i++)
         {
             tat[i]=ct[i];
             cout<<tat[i]<<endl;
+            average=average+tat[i];
         }
+        cout<<"Average Turn around time:"<<average/n<<endl;
     }
     void findWaitingTime(int process[],int n,int ct[],int bt[],int wt[])
     {   
-        int tat[n];
+        int tat[n],average=0;
         cout<<"Waiting Time:"<<endl;
         for(int i=0;i<n;i++)
         {
              tat[i]=ct[i];
             wt[i]=tat[i]-bt[i];
+            average=average+wt[i];
             cout<<wt[i]<<endl;
         }
+        average=average/n;
+        cout<<"Average waiting time:"<<average;
     }
 };
 int main()
@@ -48,17 +53,13 @@ int main()
     for(int i=1;i<=n;i++)
     {
         cin>>process[i];
-    }
-    cout<<"Process No.:"<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cout<<process[i]<<endl;
-    }
+    } 
     int burst_time[]={2,3,4,5};
     int comp_time[n],waiting_time[n],tat[n];
     f.findCompletionTime(process,n,burst_time,comp_time);
     f.findTurnAroundTime(process,n,comp_time);
     f.findWaitingTime(process,n,comp_time,burst_time,waiting_time);
+    return 0;
 
 
 }
